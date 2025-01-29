077f388a849d6d360822ec2e3b77e45664dbd0e8
Anna Zakhodyakina <kimanzo2000@gmail.com>
2025-01-29 22:22:25 +0700

    added data.py, elements_page.py, elements_page_locators.py, elements_test.py, generator.py

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,18 @@
+
+
+class TextBoxPageLocators:
+
+    # form fields - форма для заполнения
+
+    FULL_NAME = ('xpath', '//input[@id="userName"]')
+    EMAIL = ('xpath', '//input[@id="userEmail"]')
+    CURRENT_ADDRESS = ('xpath', '//textarea[@id="currentAddress"]')
+    PERMANENT_ADDRESS = ('xpath', '//textarea[@id="permanentAddress"]')
+    SUBMIT = ('xpath', '//button[@id="submit"]')
+
+    # created form - заполненная форма
+
+    CREATED_FULL_NAME = ('xpath', '//p[@id="name"]')
+    CREATED_EMAIL = ('xpath', '//p[@id="email"]')
+    CREATED_CURRENT_ADDRESS = ('xpath', '//p[@id="currentAddress"]')
+    CREATED_PERMANENT_ADDRESS = ('xpath', '//p[@id="permanentAddress"]')
