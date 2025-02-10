f86a48f726c953c66d56fbc78dfa82fd73418889
Anna Zakhodyakina <kimanzo2000@gmail.com>
2025-02-10 13:59:10 +0700

    added widgets_page.py, widgets_page_locators.py, widgets_test.py test: TestAccordianPage

---
+++ locators/widgets_page_locators.py
@@ -0,0 +1,9 @@
+
+class AccordianPageLocators:
+
+    SECTION_FIRST = ('xpath', '//div[@id="section1Heading"]')
+    SECTION_CONTENT_FIRST = ('xpath', '//div[@id="section1Content"]')
+    SECTION_SECOND = ('xpath', '//div[@id="section2Heading"]')
+    SECTION_CONTENT_SECOND = ('xpath', '//div[@id="section2Content"]')
+    SECTION_THIRD = ('xpath', '//div[@id="section3Heading"]')
+    SECTION_CONTENT_THIRD = ('xpath', '//div[@id="section3Content"]')
