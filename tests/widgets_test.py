340358d1269bf6057687e12bd55dcb774ea6af83
Anna Zakhodyakina <kimanzo2000@gmail.com>
2025-02-10 14:12:02 +0700

    added negative messages for test

--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -12,6 +12,6 @@ class TestWidgets:
             first_title, first_content = accordian_page.check_accordian('first')
             second_title, second_content = accordian_page.check_accordian('second')
             third_title, third_content = accordian_page.check_accordian('third')
-            assert first_title == 'What is Lorem Ipsum?' and first_content > 0
-            assert second_title == 'Where does it come from?' and second_content > 0
-            assert third_title == 'Why do we use it?' and third_content > 0
+            assert first_title == 'What is Lorem Ipsum?' and first_content > 0, 'The title or content is incorrect in the first accordian.'
+            assert second_title == 'Where does it come from?' and second_content > 0, 'The title or content is incorrect in the second accordian.'
+            assert third_title == 'Why do we use it?' and third_content > 0, 'The title or content is incorrect in the third accordian.'
