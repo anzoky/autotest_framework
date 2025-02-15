4b148a450c5bcc8053b72b82a8f001dd3ddaed4a
Anna Zakhodyakina <kimanzo2000@gmail.com>
2025-02-15 15:51:26 +0700

    the test TestAccordianPage has been modified and parameterization has been added

--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from conftest import driver
 from pages.widgets_page import AccordianPage, AutoCompletePage, DataPickerPage, SliderPage, ProgressBarPage, TabsPage
 
@@ -8,15 +10,17 @@ class TestWidgets:
 
     class TestAccordianPage:
 
-        def test_accordian(self, driver):
+        @pytest.mark.parametrize('accordian_type, expected_title', [
+            ('first', 'What is Lorem Ipsum?'),
+            ('second', 'Where does it come from?'),
+            ('third', 'Why do we use it?')
+        ])
+        def test_accordian(self, driver, accordian_type, expected_title):
             accordian_page = AccordianPage(driver, 'https://demoqa.com/accordian')
             accordian_page.open()
-            first_title, first_content = accordian_page.check_accordian('first')
-            second_title, second_content = accordian_page.check_accordian('second')
-            third_title, third_content = accordian_page.check_accordian('third')
-            assert first_title == 'What is Lorem Ipsum?' and first_content > 0, 'The title or content is incorrect in the first accordian.'
-            assert second_title == 'Where does it come from?' and second_content > 0, 'The title or content is incorrect in the second accordian.'
-            assert third_title == 'Why do we use it?' and third_content > 0, 'The title or content is incorrect in the third accordian.'
+            title, content_length = accordian_page.check_accordian(accordian_type)
+            assert title == expected_title, f'Incorrect title for {accordian_type} accordian.'
+            assert content_length > 0, f"Content is missing for {accordian_type} accordian."
 
     class TestAutoCompletePage:
 
